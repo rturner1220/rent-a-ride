209c510a888ed57e6a465f07d12d2a79986d809b
PGCranford <PGCranford@gmail.com>
2022-08-04 19:37:14 -0400

    renter seeds

---
+++ seeds/renter-seeds.js
@@ -0,0 +1,25 @@
+const { Renter } = require('../models')
+
+const renterData = [
+    {
+        renterName: 'Steve Mcqueen',
+        email: 'CoolGuy@testemail.com',
+        password: 'TCrown'
+    },
+    {
+        renterName: 'Elroy Blues',
+        email: 'Drytoast@testemail.com',
+        password: 'MissionFromGod'
+    },
+    {
+        renterName: 'Jeffery Lebowski',
+        email: 'TheDude@testemail.com',
+        password: 'TiestheRoomTogether'
+    }
+]
+
+
+
+const seedRenter = () => Renter.bulkCreate(renterData);
+
+module.exports = seedRenter;
