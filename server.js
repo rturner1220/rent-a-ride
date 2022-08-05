6f2f88c4bb5a6f3f1a1f5759e7e514ef1f5bb877
EstebanLVB <estv011v2@gmail.com>
2022-08-04 20:00:09 -0400

    HTML Started

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
