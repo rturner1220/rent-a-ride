de93e662e987ec31463692712e0ca96509a2aee3
PGCranford <PGCranford@gmail.com>
2022-08-05 19:59:21 -0400

    errror

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
- HEAD
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
@@ -13,13 +13,13 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //turn the routes on
 app.use(routes);
 
 //turn on connection to db and sever
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
 });
 
