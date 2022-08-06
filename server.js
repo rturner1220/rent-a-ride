beeb4cc0b4317546eba91ed406b74c9a78095e64
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-05 20:09:56 -0400

    added handlebars

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+ HEAD
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -15,3 +22,4 @@ app.use(routes);
 sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
 });
+
