f15f132f0b87fd3c99040c0062e629b7824cfa44
Betsy <betsy.griffith12@gmail.com>
2022-08-06 10:32:32 -0400

    added login handlebars and login.js code to their respective files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,14 @@ router.get('/', (req,res) => {
     res.render('homepage');
 });
 
+//route to the login page
+router.get('/login', (req, res) => {
+    // if (req.session.loggedIn) {
+    //     res.redirect('/');
+    //     return;
+    // }
+
+    res.render('login');
+});
+
 module.exports = router;
