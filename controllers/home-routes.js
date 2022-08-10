5d9ba24181a0f0b1b675d4de8473a3f0cb714c35
PGCranford <PGCranford@gmail.com>
2022-08-10 15:16:10 -0400

    error

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,18 +3,28 @@ const sequelize = require('../config/connection');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
+
     res.render('homepage');
     console.log(req.session);
+
+    res.render('homepage');
+
 });
 
+
 //route to login page
+
+
+
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
+
         res.redirect('/');
         return;
 
     }
     res.render('login');
 });
+
 
 module.exports = router;
