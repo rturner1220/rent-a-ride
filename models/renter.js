ad68afc518f4c59f1bb10ec97b430c3552a43131
Betsy <betsy.griffith12@gmail.com>
2022-08-12 18:36:15 -0400

    bcrypt added

--- models/renter.js
+++ models/renter.js
@@ -5,9 +5,9 @@ const sequelize = require('../config/connection');
 
 class Renter extends Model {
     //set up method to run on instance data (per user) to check password
-    // checkPassword(loginPw){
-    //     return bcrypt.compareSync(loginPw, this.password);
-    // }
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw, this.password);
+    }
 };
 
 Renter.init(
@@ -39,16 +39,16 @@ Renter.init(
         },
     },
     {
-        // hooks: {
-        //     async beforeCreate(newUserData){
-        //         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        //         return newUserData;
-        //     },
-        //     async beforeUpdate(updatedUserData){
-        //         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        //         return updatedUserData;
-        //     }
-        // },
+        hooks: {
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData){
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
