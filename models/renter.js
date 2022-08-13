f45904091762d4341126cd0d34f662c95e68984e
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-08-13 13:18:00 -0400

    vehicle.handlebars styling updated

--- models/renter.js
+++ models/renter.js
@@ -2,13 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-
 class Renter extends Model {
     //set up method to run on instance data (per user) to check password
     // checkPassword(loginPw){
     //     return bcrypt.compareSync(loginPw, this.password);
     // }
-};
+}
 
 Renter.init(
     {
@@ -53,7 +52,7 @@ Renter.init(
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'Renter'
+        modelName: 'Renter',
     }
 );
 
